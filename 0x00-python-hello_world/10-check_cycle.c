41111af0f0556f7f6fc2daf2af0a0f9db77220db
Moumen003 <moumenibrahim219@yahoo.com>
2023-10-31 04:11:16 +0200

    python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - fn name
+ * @list: varialve
+ *
+ * Return: 0 for suc
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t s = list, f = list;
+
+	while (f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s == f)
+			return (1);
+	}
+	return (0);
+}
