80663a710e85de75bf99e5ec05f71e9788f822e1
Moumen003 <moumenibrahim219@yahoo.com>
2023-10-31 20:01:18 +0200

    test

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i % 3 == 0 and i & 5 == 0:
+            print("FizzBuzz ")
+            continue
+        if i % 3 == 0:
+            print("Fizz ")
+            continue
+        if i % 5 == 0:
+            print("Buzz ")
+            continue
+        else:
+            print(i + " ")
+            continue
+         
