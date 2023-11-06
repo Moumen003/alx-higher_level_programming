9fd95b9c23e9149501cd87365aac75f085df22fb
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-06 23:02:56 +0200

    test

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,71 @@
+#include "lists.h"
+
+listint_t *rev(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * rev - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
+ *
+ * Return: A pointer to the head of the reversed list.
+ */
+listint_t *rev(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if palindrome.
+ * @head: ptr linked list.
+ *
+ * Return:  0 succes.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *t, *malo, *middle;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	t = *head;
+	while (t)
+	{
+		size++;
+		t = t->next;
+	}
+
+	t = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		t = t->next;
+
+	if ((size % 2) == 0 && t->n != t->next->n)
+		return (0);
+
+	t = t->next->next;
+	malo = malo(&t);
+	middle = malo;
+
+	t = *head;
+	while (malo)
+	{
+		if (t->n != malo->n)
+			return (0);
+		t = t->next;
+		malo = malo->next;
+	}
+	malo(&middle);
+
+	return (1);
+}
