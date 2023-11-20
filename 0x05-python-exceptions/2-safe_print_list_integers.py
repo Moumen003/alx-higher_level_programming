b67c87bc91156b13716046920c4f171db367e59c
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-20 18:13:43 +0200

    bos ba2a

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    while i is not x:
+    while i is not (x - 1):
         try:
             print("{:d}".format(my_list[i]))
             i += 1
