ee843873522a58e4fac9e1cd8304e588adfb958d
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-22 01:39:07 +0200

    sdf

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """define a class Square"""
 
+
 class Square:
     """define square"""
 
-
     def __init__(self, size):
         """constructor
-        
+
         Args:
             size: ds
         """
