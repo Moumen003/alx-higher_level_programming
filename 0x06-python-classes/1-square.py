74546ae91aa2784cd98ffc1012b96b46fec27461
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-22 01:22:30 +0200

    boskeda

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 class Square:
-   
+    """define square"""
+
+
     def __init__(self, size):
+        """constructor
+        
+        Args:
+            size: dfgh fg  s
+        """
         self.__size = size
