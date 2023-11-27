b3d6ddb49708b3086ae660a82960a4ab01d2e712
Moumen003 <moumenibrahim219@yahoo.com>
2023-11-27 15:35:03 +0200

    test

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" recatangle class"""
+
+
+class Rectangle:
+    """rectangle ya bsaha"""
+
+    def __init(self, width=0, height=0):
+        """new rec intialized.
+
+        Args:
+            width (int): ba asdfs
+            height (int): we malo
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ay ba2a ya fn"""
+        return self.__width
+
+    @property
+    def height(self):
+        """we malo ya gameel"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("el width yaba")
+        if value < 0:
+            raise ValueError("8alat yaba el rakam da")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("eh yaba el kalam da")
+        if value < 0:
+            raise ValueError("yaba enta sa2et math")
+        self.__height = value
