c4124b3e899569b9ae3fda7d881dcf2567ba703e
Moumen003 <moumenibrahim219@yahoo.com>
2023-12-11 14:30:08 +0200

    bos keda

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,8 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """make init 7aga gamda"""
-        super().__init__(size, size,x, y,id)
-
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """Return the print() of square."""
@@ -26,3 +25,22 @@ class Square(Rectangle):
         """set size"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """my args and kwargs"""
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
+
+    def __update(self, id=None, size=None,
+                 x=None, y=None):
+        """this is my fun"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
