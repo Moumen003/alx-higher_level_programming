cf4069aa8f21694d64ae23b9477619a7cf292501
Moumen003 <moumenibrahim219@yahoo.com>
2024-01-27 13:51:24 +0200

    asdf

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---making a new one
+-- making a new one
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
