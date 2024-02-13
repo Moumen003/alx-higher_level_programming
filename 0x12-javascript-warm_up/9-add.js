9a4ff16f7532626dd7816f02afaccd520b79bead
Moumen003 <moumenibrahim219@yahoo.com>
2024-02-13 04:09:11 +0200

    adsf

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
+	return a + b;
   }
-const a = Math.floor(Number(process.argv[2]));
-const b = Math.floor(Number(process.argv[3]));
-if (isNaN(a) || isNaN(b)) {
-  console.log('NaN');
-} else {
-    console.log(add(a, b))
-}
+  
+  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
